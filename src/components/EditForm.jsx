eb8afd40f1ed893f907b1b7f7e8b560de56676ef
Mahesa Radithya Priady <mahesaradithyapriady@gmail.com>
2024-11-09 15:56:22 +0700

    Update Memperbaharui halaman kelola user

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,23 +1,87 @@
-import React from 'react';
-
+import React from "react";
+import { useState, useEffect } from "react";
 function EditUserForm({ closeModal }) {
-
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [role, setRole] = useState("");
   const apiURL = import.meta.env.VITE_API_URL;
+  const id = localStorage.getItem("selectedUserId");
+  useEffect(() => {
+    const fetchUser = async () => {
+      const id = localStorage.getItem("selectedUserId");
+      try {
+        const res = await fetch(`${apiURL}/users/${id}`, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          method: "GET",
+        });
+        const result = await res.json();
+        const user = result.data;
+        setUsername(user.username);
+        setPassword(user.password);
+        setName(user.name);
+        setEmail(user.email);
+        setRole(user.role);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUser();
+  }, []);
 
-
-
+  const handleEdit = async (e) => {
+    e.preventDefault();
+    const res = await fetch(
+      `${apiURL}/users/${localStorage.getItem("selectedUserId")}`,
+      {
+        headers: {
+          "content-type": "application/json",
+        },
+        method: "PUT",
+        body: JSON.stringify({
+          username,
+          name,
+          email,
+          password,
+          role,
+        }),
+      }
+    );
+    const result = await res.json();
+    localStorage.removeItem("selectedUserId");
+    closeModal();
+  };
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
       <div className="bg-white rounded-lg p-6 w-1/2">
         <h2 className="text-xl font-bold mb-4">Edit User</h2>
-        <form>
+        <form onSubmit={handleEdit}>
+          <div className="mb-4">
+            <label className="block text-gray-700">Nama</label>
+            <input
+              type="text"
+              className="input input-bordered w-full"
+              placeholder="Masukkan Nama"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              value={name}
+            />
+          </div>
           <div className="mb-4">
             <label className="block text-gray-700">Username</label>
             <input
               type="text"
               className="input input-bordered w-full"
               placeholder="Masukkan username"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              value={username}
             />
           </div>
           <div className="mb-4">
@@ -26,25 +90,41 @@ function EditUserForm({ closeModal }) {
               type="email"
               className="input input-bordered w-full"
               placeholder="Masukkan email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
           </div>
-          <div className='mb-4'>
+          <div className="mb-4">
             <label className="block text-gray-700">Password</label>
             <input
               type="password"
               className="input input-bordered w-full"
               placeholder="Masukkan password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
             />
           </div>
           <div className="mb-4">
             <label className="block text-gray-700">Tipe User</label>
-            <select className="select select-bordered w-full">
-              <option>Apoteker</option>
-              <option>Kasir</option>
+            <select
+              className="select select-bordered w-full"
+              onChange={(e) => setRole(e.target.value)}
+            >
+              {role == "kasir" ? (
+                <>
+                  <option>Kasir</option>
+                  <option>Apoteker</option>
+                </>
+              ) : (
+                <>
+                  <option>Apoteker</option>
+                  <option>Kasir</option>
+                </>
+              )}
             </select>
           </div>
           <div className="flex justify-end">
-            <button 
+            <button
               type="button"
               className="btn btn-ghost mr-2"
               onClick={closeModal}
--- src/pages/KelolaUser.jsx
+++ src/pages/KelolaUser.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from '../components/Navbar';
-import Sidebar from '../components/Sidebar';
-import TambahUserForm from '../components/TambahUserFrom';
-import EditUserForm from '../components/EditForm';
-import DeleteForm from '../components/DeleteForm';
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import TambahUserForm from "../components/TambahUserFrom";
+import EditUserForm from "../components/EditForm";
+import DeleteForm from "../components/DeleteForm";
 
 function KelolaUser() {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
@@ -13,31 +13,35 @@ function KelolaUser() {
   const [users, setUsers] = useState([]);
   const [loggedInUserId, setLoggedInUserId] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [selectedUserId, setSelectedUserId] = useState(null);  // State untuk ID pengguna yang dipilih
+  const [selectedUserId, setSelectedUserId] = useState(null); // State untuk ID pengguna yang dipilih
 
   const apiURL = import.meta.env.VITE_API_URL;
 
   useEffect(() => {
-    const userId = localStorage.getItem('loggedInUserId');
+    const userId = localStorage.getItem("loggedInUserId");
+    const selectedId = localStorage.setItem("selectedUserId", selectedUserId);
     if (userId) {
       setLoggedInUserId(parseInt(userId));
     }
-  }, []);
+  }, [selectedUserId]);
 
   const fetchUsers = async () => {
     try {
       const res = await fetch(`${apiURL}/users`, {
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        method: 'GET',
+        method: "GET",
       });
       const data = await res.json();
       setUsers(data.data);
     } catch (error) {
-      console.error('Tidak dapat mengambil data:', error);
+      console.error("Tidak dapat mengambil data:", error);
     } finally {
-      setIsLoading(false);
+      setIsLoading(true);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 5000);
     }
   };
 
@@ -45,13 +49,19 @@ function KelolaUser() {
     fetchUsers();
   }, []);
 
-  const filteredUsers = users.filter(user => user.id !== loggedInUserId && user.role !== 'admin');
+  const filteredUsers = users.filter(
+    (user) => user.id !== loggedInUserId && user.role !== "admin"
+  );
 
   const toggleSidebar = () => setSidebarOpen(!isSidebarOpen);
   const toggleModal = () => setModalOpen(!isModalOpen);
-  const toggleEditModal = () => setEditModalOpen(!isEditModalOpen);
+  const toggleEditModal = (id) => {
+    setSelectedUserId(id);
+    setEditModalOpen(!isEditModalOpen);
+  };
+
   const toggleDeleteModal = (userId) => {
-    setSelectedUserId(userId);  // Set selected user ID saat tombol hapus diklik
+    setSelectedUserId(userId); // Set selected user ID saat tombol hapus diklik
     setDeleteModalOpen(!isDeleteModalOpen);
   };
 
@@ -59,36 +69,53 @@ function KelolaUser() {
     setModalOpen(false);
     setEditModalOpen(false);
     setDeleteModalOpen(false);
+    localStorage.removeItem("selectedUserId");
     fetchUsers(); // Refresh data saat modal ditutup
   };
 
   return (
-    <div className='flex min-h-screen'>
+    <div className="flex min-h-screen">
       <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      <div className='flex-1 flex flex-col'>
+      <div className="flex-1 flex flex-col">
         <Navbar toggleSidebar={toggleSidebar} />
-        <main className='p-4 overflow-y-auto bg-slate-300 flex-1'>
-          <div className='mx-20'>
-            <h1 className='text-3xl font-sans font-bold text-black mt-10'>Kelola User</h1>
+        <main className="p-4 overflow-y-auto bg-slate-300 flex-1">
+          <div className="mx-20">
+            <h1 className="text-3xl font-sans font-bold text-black mt-10">
+              Kelola User
+            </h1>
           </div>
-          <div className='mt-10 flex justify-end'>
-            <button className='btn btn-primary text-white text-md mr-5' onClick={toggleModal}>
+          <div className="mt-10 flex justify-end">
+            <button
+              className="btn btn-primary text-white text-md mr-5"
+              onClick={toggleModal}
+            >
               Tambah User
             </button>
           </div>
 
           {isModalOpen && <TambahUserForm closeModal={handleModalClose} />}
           {isEditModalOpen && <EditUserForm closeModal={handleModalClose} />}
-          {isDeleteModalOpen && <DeleteForm closeModal={handleModalClose} userId={selectedUserId} refreshData={fetchUsers} />}
-          
-          <div className='mt-10 mx-auto bg-white rounded-md w-full'>
+          {isDeleteModalOpen && (
+            <DeleteForm
+              closeModal={handleModalClose}
+              userId={selectedUserId}
+              refreshData={fetchUsers}
+            />
+          )}
+
+          <div className="mt-10 mx-auto bg-white rounded-md w-full">
             <div className="overflow-x-auto">
               {isLoading ? (
-                <div className="p-4 space-y-4">
-                  {/* Skeleton loading */}
+                <div className="loadingSekeleton p-5">
+                  <div className="skeleton h-5 w-full mb-5"></div>
+                  <div className="loadingSekeletonRows flex">
+                    <div className="skeleton h-12 w-[1090px] flex flex-column"></div>
+                    <div className="skeleton h-12 w-16 ml-2 flex flex-column"></div>
+                    <div className="skeleton h-12 w-16 ml-2 flex flex-column"></div>
+                  </div>
                 </div>
               ) : (
-                <table className='table table-zebra'>
+                <table className="table table-zebra">
                   <thead>
                     <tr>
                       <th>Id_User</th>
@@ -99,24 +126,52 @@ function KelolaUser() {
                     </tr>
                   </thead>
                   <tbody>
-                    {filteredUsers.map(user => (
+                    {filteredUsers.map((user) => (
                       <tr key={user.id}>
                         <td>{user.id}</td>
                         <td>{user.username}</td>
                         <td>{user.role}</td>
                         <td>{user.email}</td>
-                        <td className='flex'>
-                          <button className='btn btn-square btn-primary text-white text-md mr-2' onClick={toggleEditModal}>
-                          <svg class="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-  <path fill-rule="evenodd" d="M5 8a4 4 0 1 1 7.796 1.263l-2.533 2.534A4 4 0 0 1 5 8Zm4.06 5H7a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h2.172a2.999 2.999 0 0 1-.114-1.588l.674-3.372a3 3 0 0 1 .82-1.533L9.06 13Zm9.032-5a2.907 2.907 0 0 0-2.056.852L9.967 14.92a1 1 0 0 0-.273.51l-.675 3.373a1 1 0 0 0 1.177 1.177l3.372-.675a1 1 0 0 0 .511-.273l6.07-6.07a2.91 2.91 0 0 0-.944-4.742A2.907 2.907 0 0 0 18.092 8Z" clip-rule="evenodd"/>
-</svg>
-
+                        <td className="flex">
+                          <button
+                            className="btn btn-square btn-primary text-white text-md mr-2"
+                            onClick={() => toggleEditModal(user.id)}
+                          >
+                            <svg
+                              className="w-6 h-6 text-white dark:text-white"
+                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="24"
+                              height="24"
+                              fill="currentColor"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M5 8a4 4 0 1 1 7.796 1.263l-2.533 2.534A4 4 0 0 1 5 8Zm4.06 5H7a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h2.172a2.999 2.999 0 0 1-.114-1.588l.674-3.372a3 3 0 0 1 .82-1.533L9.06 13Zm9.032-5a2.907 2.907 0 0 0-2.056.852L9.967 14.92a1 1 0 0 0-.273.51l-.675 3.373a1 1 0 0 0 1.177 1.177l3.372-.675a1 1 0 0 0 .511-.273l6.07-6.07a2.91 2.91 0 0 0-.944-4.742A2.907 2.907 0 0 0 18.092 8Z"
+                                clip-rule="evenodd"
+                              />
+                            </svg>
                           </button>
-                          <button className='btn btn-square btn-error text-white text-md' onClick={() => toggleDeleteModal(user.id)}>
-                          <svg class="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-  <path fill-rule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd"/>
-</svg>
-
+                          <button
+                            className="btn btn-square btn-error text-white text-md"
+                            onClick={() => toggleDeleteModal(user.id)}
+                          >
+                            <svg
+                              className="w-6 h-6 text-white dark:text-white"
+                              aria-hidden="true"
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="24"
+                              height="24"
+                              fill="currentColor"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z"
+                                clip-rule="evenodd"
+                              />
+                            </svg>
                           </button>
                         </td>
                       </tr>
